693bb363529fafeca2c8a093bd5a0e8c03697e7f
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-09 22:40:19 -0600

    added children prop to be able to customize my buttons but keep consistent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Button from "./Button";
+
 function App() {
-    return <div>hello there...</div>;
-}
+    return (
+        <div>
+            <div>
+                <Button>Hello</Button>
+            </div>
+            <div>
+                <Button>Test Me</Button>
+            </div>
+            <div>
+                <Button>Buy Now</Button>
+            </div>
+            <div>
+                <Button>CLICK HERE</Button>
+            </div>
+            <div>
+                <Button>Email ME</Button>
+            </div>
+            <div>
+                <Button>Search</Button>
+            </div>
+        </div>
+    )
+};
 
 export default App;
