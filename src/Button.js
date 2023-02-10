ae169fd2c192f513f4d78c168501e5801a0b0f35
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-09 23:27:04 -0600

    added validation for various button props

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,34 @@
-function Button ({ children }) {
+import PropTypes from 'prop-types';
+
+function Button({
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded
+}) {
+    // if (primary && secondary) {
+    //     throw new Error('only one of the primary and secondary should be provided')
+    // }
+
     return <button>{children}</button>
 }
+
+Button.propTypes = {
+    checkVariatonValue: ({ primary, secondary, warning, success, danger }) => {
+        const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!warning)
+        + Number(!!success)
+        + Number(!!danger)
+    
+        if (count > 1) {
+            return new Error('Only one of primary, secondary, warning, success, danger can be true')
+        }
+    },
+};
 
 export default Button;
