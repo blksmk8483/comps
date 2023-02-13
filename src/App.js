4e86f74c93e8e24dbe63b84e2c1527a0c6d7237d
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-13 10:12:05 -0600

    added Accordion.js to components, added an items array to App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import Button from "./components/Button";
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go'
+import Accordion from "./components/Accordion";
 
 function App() {
-    const handleClick = () => {
-        console.log('You clicked me!')
-    }
-    
-    return (
-        <div>
-            <div>
-                <Button success rounded outline className="mb-5" onClick={handleClick}>
-                    < GoBell />
-                    Hello
-                    </Button>
-            </div>
-            <div>
-                <Button danger outline >
-                    < GoCloudDownload />
-                    Test Me
-                    </Button>
-            </div>
-            <div>
-                <Button warning >
-                    < GoDatabase />
-                    Buy Now
-                    </Button>
-            </div>
-            <div>
-                <Button secondary outline>CLICK HERE</Button>
-            </div>
-            <div>
-                <Button primary rounded>Search</Button>
-            </div>
-        </div>
-    )
+    const items = [
+        {
+            label: 'Can I use React on a project?',
+            content: 'You can use React on any project you want.'
+        },
+        {
+            label: 'Can I use JavaSript on a project?',
+            content: 'You can use JavaScript on any project you want.'
+        },
+        {
+            label: 'Can I use CSS on a project?',
+            content: 'You can use CSS on any project you want.'
+        }
+    ]
+
+    return <Accordion items={items}/>;
 };
 
 export default App;
