6388abd3f445919d74b7f1b0d61df987b3424211
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-14 11:55:08 -0600

    added the id in my items array, added logic for teh accordion expanded/collapsed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Accordion from "./components/Accordion";
 function App() {
     const items = [
         {
+            id: '123a',
             label: 'Can I use React on a project?',
             content: 'You can use React on any project you want.'
         },
         {
+            id: '1gfdwa',
             label: 'Can I use JavaSript on a project?',
             content: 'You can use JavaScript on any project you want.'
         },
         {
+            id: '12dfda',
             label: 'Can I use CSS on a project?',
             content: 'You can use CSS on any project you want.'
         }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 function Accordion({ items }) {
-    return <div />
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        if (index === expandedIndex) {
+            console.log('expanded');
+        } else {
+            console.log('collapsed');
+        }
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        )
+    })
+
+    return <div>{renderedItems}</div>
+
 }
 
 export default Accordion;
