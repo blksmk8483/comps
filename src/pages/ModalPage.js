c44657599e4479ca1a1ccfb4e4489ccbb0c0262b
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-20 17:27:56 -0600

    function to close the modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -9,12 +9,16 @@ function ModalPage() {
         setShowModal(true);
     };
 
+    const handleClose = () => {
+        setShowModal(false);
+    }
+
     return (
         <div className='relative'>
             <Button onClick={handleClick} primary>
                 Open Modal
                 </Button>
-            {showModal && <Modal />}
+            {showModal && <Modal onClose={handleClose}/>}
         </div>
     );
 };
