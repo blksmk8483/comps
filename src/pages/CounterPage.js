aba91df27e9a156bfbbdd2f965594618921b4ed5
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-22 22:46:51 -0600

    made a custom hook (use-counter) to clean up CounterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,22 +1,16 @@
-import { useState, useEffect } from "react";
 import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
 
-function CounterPage({ initialCount }) {
-    const [count, setCount] = useState(initialCount);
 
-    useEffect(() => {
-        console.log(count);
 
-    }, [count]);
-
-    const handleClick = () => {
-        setCount(count + 1);
-    };
+ function CounterPage({ initialCount }) {
+    const { count, increment } = useCounter(initialCount);
+   
 
     return ( 
     <div>
     <h1>Count is {count}</h1>
-    <Button onClick={handleClick} >
+    <Button onClick={increment} >
         Increment
     </Button>
     </div>
