97368fe27831fec5b46fda3a9f6b629171594640
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-24 12:08:15 -0600

    added in the other state functions back in using useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -3,7 +3,10 @@ import Button from '../components/Button';
 import Panel from '../components/Panel';
 
 const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
+
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -12,10 +15,21 @@ const reducer = (state, action) => {
                 ...state,
                 count: state.count + 1
             };
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            };
         case SET_VALUE_TO_ADD:
             return {
                 ...state,
                 valueToAdd: action.payload
+            };
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0
             };
         default:
             return state;
@@ -33,9 +47,13 @@ function CounterPage({ initialCount }) {
             type: INCREMENT_COUNT
         });
     };
+
     const decrement = () => {
-        // setCount(count - 1);
+        dispatch({
+            type: DECREMENT_COUNT
+        })
     };
+
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
 
@@ -47,6 +65,11 @@ function CounterPage({ initialCount }) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        dispatch({
+            type: ADD_VALUE_TO_COUNT
+        })
+
     };
 
     return (
